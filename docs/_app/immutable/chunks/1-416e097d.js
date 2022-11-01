9c22d2986b22760ae15a117825e173ece850be4c
Simon Green <simon@captaincodeman.com>
2022-11-01 00:01:39 -0600

    update docs

---
+++ docs/_app/immutable/chunks/1-416e097d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ec289378.js";export{t as component};
--- docs/_app/immutable/chunks/2-08ff709c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-4765e6e4.js";export{t as component};
