48a9b1ab31d07d2b6acb09f871b74d6897101329
Simon Green <simon@captaincodeman.com>
2023-06-01 10:43:59 -0600

    update
    prep for svelte 4.x
    update dependencies
    deploy docs with github action

--- docs/_app/immutable/chunks/2-4aa7c7fb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-143efd1e.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,18 @@
 import adapter from '@sveltejs/adapter-static'
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const prod = process.env.NODE_ENV === 'production'
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-		}),
+		adapter: adapter(),
 		paths: {
-			base: prod ? '/svelte-color-select' : '',
+			base: dev ? '' : process.env.BASE_PATH,
 		},
 	},
 }
