a3bb424532f13c250000c76ec7fbfa0db68574b1
Simon Green <simon@captaincodeman.com>
2023-08-27 19:30:11 -0600

    use svelte-package
    tsup / esbuild were all problematic so the pragmatic solution is to forget automated minification of the glsl and just include it as string literals
    also update dependencies

---
+++ src/shaders/readme.md
@@ -0,0 +1,2 @@
+shaders adapted from https://github.com/dokozero/okcolor
+the source in this folder is minified and included in a ts file to enable svelte-package to be used
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import glsl from 'vite-plugin-glsl'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-	switch (mode) {
-		case 'development':
-		case 'production':
-			return {
-				plugins: [
-					glsl(),
-					sveltekit()
-				]
-			}
-		case 'library':
-			return {
-				plugins: [
-					glsl({ compress: true }),
-					svelte({ emitCss: false })
-				],
-				build: {
-					lib: {
-						entry: 'src/lib/index.ts',
-						formats: ['es'],
-						fileName: 'index',
-					}
-				}
-			}
-		default: throw 'unknown mode'
-	}
+export default defineConfig({
+	plugins: [sveltekit()]
 })
