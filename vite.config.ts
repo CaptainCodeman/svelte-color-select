55c7bb1a12f2baffe1744f52ef85f43b33b67430
Simon Green <simon@captaincodeman.com>
2023-08-28 09:10:19 -0600

    auto minify the glsl when packaging
    a bit gnarly, but it works

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import glsl from 'vite-plugin-glsl'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		glsl({ compress: true }),
+		sveltekit(),
+	]
 })
